c0243e732dff10f7a701191472f6d1a9c1a84d4e
= <=>
2020-12-05 21:39:59 -0500

    New depl

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,11 @@
 // Countdown to event date
-const countDown = (eleMonthsId, eleDaysId, EleHoursId, EleMinutesId, eDate) => {
+const countDown = (
+  eleDaysId,
+  EleHoursId,
+  EleMinutesId,
+  eleSecondsId,
+  eDate
+) => {
   // Current date in ms
   const currentDate = new Date(Date.now());
   // Event date in ms
@@ -7,31 +13,24 @@ const countDown = (eleMonthsId, eleDaysId, EleHoursId, EleMinutesId, eDate) => {
   // Remaining time to event date in ms
   const remainTime = eventDate - currentDate;
 
-  // Number of months until the event
-  const monthToMs = 30 * 24 * 60 * 60 * 1000;
-  const months = Math.floor(remainTime / monthToMs);
   // Number of days until the event
   const dayToMs = 24 * 60 * 60 * 1000;
-  const days = Math.floor((remainTime - months * monthToMs) / dayToMs);
+  const days = Math.floor(remainTime / dayToMs);
   // Number of hours until the event
   const hourToMs = 60 * 60 * 1000;
-  const hours = Math.floor(
-    (remainTime - months * monthToMs - days * dayToMs) / hourToMs
-  );
+  const hours = Math.floor((remainTime - days * dayToMs) / hourToMs);
   // Number of minutes until the event
   const minuteToMs = 60 * 1000;
   const minutes = Math.floor(
-    (remainTime - months * monthToMs - days * dayToMs - hours * hourToMs) /
-      minuteToMs
+    (remainTime - hours * hourToMs - days * dayToMs) / minuteToMs
+  );
+  // Number of seconds until the event
+  const secondToMs = 1000;
+  const seconds = Math.floor(
+    (remainTime - minutes * minuteToMs - days * dayToMs - hours * hourToMs) /
+      secondToMs
   );
 
-  document.getElementById(eleMonthsId).innerHTML = months.toLocaleString(
-    'en-US',
-    {
-      minimumIntegerDigits: 2,
-      useGrouping: false
-    }
-  );
   document.getElementById(eleDaysId).innerHTML = days.toLocaleString('en-US', {
     minimumIntegerDigits: 2,
     useGrouping: false
@@ -47,89 +46,81 @@ const countDown = (eleMonthsId, eleDaysId, EleHoursId, EleMinutesId, eDate) => {
       useGrouping: false
     }
   );
+  document.getElementById(eleSecondsId).innerHTML = seconds.toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  });
 
   setTimeout(() => {
-    countDown(eleMonthsId, eleDaysId, EleHoursId, EleMinutesId, eDate);
+    countDown(eleDaysId, EleHoursId, EleMinutesId, eleSecondsId, eDate);
   }, 1000);
 };
 
-const scrollSpy = () => {
-  let home = document.getElementById('home');
-  let about = document.getElementById('about');
-  let tracks = document.querySelector('#tracks');
-  let schedule = document.querySelector('#schedule');
-  let sponsors = document.querySelector('#sponsors');
-  // let faq = document.querySelector('#faq');
-  // let contact = document.querySelector('#contact');
-
-  let links = document.querySelectorAll('.nav-link');
-  let homeLink = document.querySelectorAll(`a[href='#home']`);
-  let aboutLink = document.querySelectorAll(`a[href='#about']`);
-  let tracksLink = document.querySelectorAll(`a[href='#tracks']`);
-  let scheduleLink = document.querySelectorAll(`a[href='#schedule']`);
-  let sponsorsLink = document.querySelectorAll(`a[href='#sponsors']`);
-  // let faqLink = document.querySelectorAll(`a[href='#faq']`);
-  // let contactLink = document.querySelectorAll(`a[href='#contact']`);
-
-  window.addEventListener('scroll', () => {
-    var windo = window.pageYOffset + 400;
-
-    if ((windo >= home.offsetTop) & (windo < about.offsetTop)) {
-      for (const link of links) {
-        link.classList.remove('active');
-      }
-      homeLink[0].classList.add('active');
-    }
-    if ((windo >= about.offsetTop) & (windo < tracks.offsetTop)) {
-      for (const link of links) {
-        link.classList.remove('active');
-      }
-      aboutLink[0].classList.add('active');
-    }
-    if ((windo >= tracks.offsetTop) & (windo < schedule.offsetTop)) {
-      for (const link of links) {
-        link.classList.remove('active');
-      }
-      tracksLink[0].classList.add('active');
-    }
-    if ((windo >= schedule.offsetTop) & (windo < sponsors.offsetTop)) {
-      for (const link of links) {
-        link.classList.remove('active');
-      }
-      scheduleLink[0].classList.add('active');
-    }
-    // if ((windo >= sponsors.offsetTop) & (windo < faq.offsetTop)) {
-    //   for (const link of links) {
-    //     link.classList.remove('active');
-    //   }
-    //   sponsorsLink[0].classList.add('active');
-    // }
-    // if ((windo >= faq.offsetTop) & (windo < contact.offsetTop)) {
-    //   for (const link of links) {
-    //     link.classList.remove('active');
-    //   }
-    //   faqLink[0].classList.add('active');
-    // }
-    // if (windo >= contact.offsetTop) {
-    //   for (const link of links) {
-    //     link.classList.remove('active');
-    //   }
-    //   contactLink[0].classList.add('active');
-    // }
-    if (windo >= sponsors.offsetTop) {
-      for (const link of links) {
-        link.classList.remove('active');
-      }
-      sponsorsLink[0].classList.add('active');
-    }
-  });
-};
-
 document.addEventListener('DOMContentLoaded', () => {
-  countDown('months', 'days', 'hours', 'minutes', new Date(2021, 01, 13));
+  countDown('days', 'hours', 'minutes', 'seconds', new Date(2021, 01, 13));
 });
 
-scrollSpy();
+// Scroll to top
+var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+scrollToTopBtn.addEventListener('click', scrollToTop);
+
+// Track switching
+var data = {
+  track1: {
+    title: 'Track Title 1',
+    content:
+      'Track Content 1, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
+  },
+  track2: {
+    title: 'Track Title 2',
+    content:
+      'Track Content 2, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
+  },
+  track3: {
+    title: 'Track Title 3',
+    content:
+      'Track Content 3, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
+  }
+};
+
+function switchClick(track) {
+  var title = document.getElementById('trackTitle');
+  var content = document.getElementById('trackContent');
+  var icon1 = document.getElementById('trackIcon1');
+  var icon2 = document.getElementById('trackIcon2');
+  var icon3 = document.getElementById('trackIcon3');
+
+  if (track === 'track1') {
+    title.innerHTML = data.track1.title;
+    content.innerHTML = data.track1.content;
+    icon1.classList.add('active');
+    icon2.classList.remove('active');
+    icon3.classList.remove('active');
+  }
+  if (track === 'track2') {
+    title.innerHTML = data.track2.title;
+    content.innerHTML = data.track2.content;
+    icon1.classList.remove('active');
+    icon2.classList.add('active');
+    icon3.classList.remove('active');
+  }
+  if (track === 'track3') {
+    title.innerHTML = data.track3.title;
+    content.innerHTML = data.track3.content;
+    icon1.classList.remove('active');
+    icon2.classList.remove('active');
+    icon3.classList.add('active');
+  }
+}
 
 // Init AOS
 AOS.init();
